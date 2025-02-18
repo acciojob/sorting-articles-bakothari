c65dd917131c3db9f908a284ea5288be2e544471
Bhavya Kothari <129605972+bakothari@users.noreply.github.com>
2025-02-18 09:27:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+const bands = [
+            'The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled',
+            'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper',
+            'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'
+        ];
+
+        const articles = ['A ', 'An ', 'The '];
+
+        const sortedBands = bands.sort((a, b) => {
+            for (let article of articles) {
+                if (a.startsWith(article)) a = a.slice(article.length);
+                if (b.startsWith(article)) b = b.slice(article.length);
+            }
+            return a.localeCompare(b);
+        });
+
+        document.getElementById('bands').innerHTML = 
+            sortedBands.map(band => `<li>${band}</li>`).join('');
